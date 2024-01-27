311c8687b3f3562e341e1a1314a610d527efde52
IrinaB <balkarovairina@gmail.com>
2024-01-27 10:59:57 +0100

    Add Fetching Cabin Data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,17 +2,23 @@ import Heading from '../ui/Heading'
 import Row from '../ui/Row'
 import { useEffect } from 'react'
 import { getCabins } from '../services/apiCabins.js'
+import CabinTable from '../features/cabins/CabinTable.jsx'
 
 function Cabins() {
     useEffect(() => {
-        getCabins().then((data) => console.log(data))
+        getCabins().then((data) => console.log())
     }, [])
     return (
-        <Row type="horizontal">
-            <Heading as="h1">All cabins</Heading>
-            <p>TEST</p>
-            <img src='https://fuzggbzflowghqnnelyd.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg' alt='' />
-        </Row>
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">All cabins</Heading>
+                <p>Filter / Sort</p>
+                {/*<img src='https://fuzggbzflowghqnnelyd.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg' alt='' />*/}
+            </Row>
+            <Row>
+                <CabinTable />
+            </Row>
+        </>
     )
 }
 
